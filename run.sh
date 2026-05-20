9d038f74d3d16fdc85dbe3088d8555412c9eda88
chris-purpleotter <chris@purpleotter.ca>
2026-05-20 17:33:15 -0400

    remove parquet external table creation

--- run.sh
+++ run.sh
@@ -2,8 +2,5 @@
 # Exit immediately if any command in this script throws an error
 set -e  
 
-echo "=== STEP 1: Refreshing GCS BigLake External Tables ==="
-dbt run-operation stage_external_sources
-
-echo "=== STEP 2: Running Staging Models and Data Quality Tests ==="
+echo "=== Running Staging Models and Data Quality Tests ==="
 dbt build
